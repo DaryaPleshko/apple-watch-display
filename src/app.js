7bd9ee62ac430730ca813414ad75adc92b0ef5ee
Darya Pleshko <daryapleshko.work.dev@mail.ru>
2023-02-04 16:28:36 +0300

    commit

--- src/app.js
+++ src/app.js
@@ -1,19 +1,16 @@
-function clockTimer() {
+const clockTimer = () => {
+    const clock = document.querySelector(".clock");
+    const yearInRealLife = document.querySelector(".year");
+
     const date = new Date();
-
     let time = [date.getHours(), date.getMinutes(), date.getFullYear()];
-
 
     if (time[0] < 10) time[0] = `0${time[0]}`;
     if (time[1] < 10) time[1] = `0${time[1]}`;
     const current_time = [time[0], time[1]].join(':');
 
-    const clock = document.querySelector(".clock");
-    const yearInRealLife = document.querySelector(".year");
-
     clock.innerHTML = current_time;
     yearInRealLife.innerHTML = time[2];
-
     setTimeout("clockTimer()", 1000);
 }
 
